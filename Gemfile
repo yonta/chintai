35a1c27d62ec06681abdf7dccb52b091db24e819
SAITOU Keita <keita44.f4@gmail.com>
2023-05-11 22:02:48 +0900

    Update get script to output pastable line for spreadsheet

--- Gemfile
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
-
-gem "google-apis-sheets_v4"
-gem "googleauth"
--- get.rb
+++ get.rb
@@ -1,62 +1,38 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require "yaml"
-require "googleauth"
-require "google/apis/sheets_v4"
-require "googleauth/stores/file_token_store"
-require "fileutils"
+require "open-uri"
+require "nokogiri"
 
-APPLICATION_NAME = "[YOUR APPLICATION NAME IN GOOGLE CLOUD]"
-# Download client secret from google cloud platform
-# https://console.cloud.google.com/
-CREDENTIALS_PATH = "./client_secret.json"
-SHEET_ID = "[YOUR SHEET ID LIKE AsDfGk..]"
-SHEET_NAME = "[YOUR SEET NAME LIKE sheet1]"
+# test input
+url = "https://www.homes.co.jp/chintai/b-1028850275072/"
+html = URI.parse(url).open.read
+doc = Nokogiri::HTML.parse(html)
 
-OOB_URI = "urn:ietf:wg:oauth:2.0:oob"
-TOKEN_PATH = "token.yaml"
-SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS
+# name
+name_base = doc.at_css(".bukkenHead .heading .bukkenName")&.text&.strip
+name_ext = doc.at_css(".bukkenHead .heading .bukkenRoom")&.text&.strip
+name = "#{name_base} #{name_ext}"
 
-##
-# Ensure valid credentials, either by restoring from the saved credentials
-# files or intitiating an OAuth2 authorization. If authorization is required,
-# the user's default browser will be launched to approve the request.
-#
-# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
-def authorize
-  client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
-  token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
-  authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-  user_id = "default"
-  credentials = authorizer.get_credentials(user_id)
-  if credentials.nil?
-    url = authorizer.get_authorization_url(base_url: OOB_URI)
-    puts "Open the following URL in the browser and enter the " \
-         "resulting code after authorization:\n" + url
-    code = gets
-    credentials = authorizer.get_and_store_credentials_from_code(
-      user_id: user_id, code: code, base_url: OOB_URI
-    )
-  end
-  credentials
-end
+# address
+address = doc.at_css(".bukkenSpec .rentLocation #chk-bkc-fulladdress")&.text&.strip&.split("\n")&.first
 
-# Initialize the API
-service = Google::Apis::SheetsV4::SheetsService.new
-service.client_options.application_name = APPLICATION_NAME
-service.authorization = authorize
+# rental fee
+rent = doc.at_css(".bukkenSpec #chk-bkc-moneyroom")&.text&.strip
+rent_match = rent&.match(/(?<base>[\d\.]+)万円 \( (?<ext>[\d,]+)円 \)/)
+rent_base = rent_match[:base].to_f
+rent_ext = rent_match[:ext].delete(",").to_f
+rent_total = rent_base + rent_ext * 0.0001
 
-range = "#{SHEET_NAME}!A1:B1"
+# floor area
+floor_area = doc.at_css(".bukkenSpec #chk-bkc-housearea")&.text&.strip.match(/(?<area>[\d\.]+)m²/)[:area]
 
-# read spreadsheet
-response = service.get_spreadsheet_values(sheet_id, range)
-pp response
+# floor plan
+floors = doc.at_css(".bukkenSpec #chk-bkc-marodi")&.text&.strip
+floor_plan = floors&.split("\n").first
+floor_ldk = floors.match(/リビングダイニングキッチン\s*(?<ldk>[\d\.]+)帖/)[:ldk].to_f
+floor_rooms = floors.match(/洋室\s*(?<room1>[\d\.]+)帖.*洋室\s*(?<room2>[\d\.]+)帖/m)
+floor_room1 = floor_rooms[:room1]
+floor_room2 = floor_rooms[:room2]
 
-# write spreadsheet
-values = [[2, 2]]
-service.update_spreadsheet_value(
-  SHEET_ID,
-  range,
-  { values: values },
-  value_input_option: "USER_ENTERED"
-)
+print "#{name},,,#{url},#{address},,,,#{rent_total},#{floor_area},#{floor_ldk},#{floor_room1},,#{floor_room2}\n"
